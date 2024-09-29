79364b6c18b6a45a02be18c2dc62b36da559bd66
HOJOONKIM <k296070@snu.ac.kr>
2024-09-29 17:38:39 +0900

    added script

--- scripts/test-4bit.sh
+++ scripts/test-4bit.sh
@@ -1,4 +1,4 @@
-python ./examples/quant_model_rtn.py \
+CUDA_VISIBLE_DEVICES=1 python ./examples/quant_model_rtn.py \
     --model_name_or_path ../model/Meta-Llama-3-8B \
     --qbits 4 \
     --group_size 128
