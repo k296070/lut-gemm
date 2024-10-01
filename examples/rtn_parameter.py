ff99ad1b141af1b1d657744c94dfac6191f2c012
HOJOONKIM <k296070@snu.ac.kr>
2024-10-01 15:07:59 +0900

    Update rtn_parameter.py

--- examples/rtn_parameter.py
+++ examples/rtn_parameter.py
@@ -78,7 +78,7 @@ class RTNParameter(CompressionParameter):
         global PACKER
 
         zero   = scale * zero
-        upack  = torch.Tensor([[2**i for i in range(qbits)]]).to(torch.device('cuda:0'))
+        upack  = torch.Tensor([[2**i for i in range(qbits)]])
         scale  = scale / 2.0
         scale  = torch.matmul(scale, upack)
 
