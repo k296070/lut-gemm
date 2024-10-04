cb35a58b4acdd79adc63e9eb0bf9794f456cad88
HOJOONKIM <k296070@snu.ac.kr>
2024-10-04 22:10:05 +0900

    Update rtn_parameter.py

--- examples/rtn_parameter.py
+++ examples/rtn_parameter.py
@@ -101,7 +101,7 @@ if __name__ == '__main__':
     # INT4 Quantization -> RTN
     w_rtn = RTNParameter(w_org)
     scale, zero, w_quant, w_quant_shape = w_rtn.compress(in_ch_wise=False, qbits=4, group_size=128, perchannel=True, sym=False)
-    w_rtn.decompress(scale, zero, w_quant, w_quant_shape, in_ch_wise=False)
+
     print(abs(w_org-w_rtn.data).mean())
 
     # Convert INT4 -> BCQ4
