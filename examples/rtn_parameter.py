16882c903cc0682995109c0cf9481a6a5d60dd84
HOJOONKIM <k296070@snu.ac.kr>
2024-10-04 22:35:03 +0900

    Update rtn_parameter.py

--- examples/rtn_parameter.py
+++ examples/rtn_parameter.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
 
     # INT4 Quantization -> RTN
     w_rtn = RTNParameter(w_org)
-    scale, zero, w_quant, w_quant_shape = w_rtn.compress(in_ch_wise=False, qbits=4, group_size=128, perchannel=True, sym=True)
+    scale, zero, w_quant, w_quant_shape = w_rtn.compress(in_ch_wise=True, qbits=4, group_size=128, perchannel=True, sym=True)
 
     print(abs(w_org-w_rtn.data).mean())
 
