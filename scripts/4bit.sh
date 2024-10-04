310b39ab06e62e7a94b0733b55ee38c09ccfbfac
HOJOONKIM <k296070@snu.ac.kr>
2024-10-04 20:53:41 +0900

    edit

--- scripts/4bit.sh
+++ scripts/4bit.sh
@@ -1,5 +1,13 @@
-CUDA_VISIBLE_DEVICES=1 python ./examples/quant_model_rtn.py \
-    --model_name_or_path ../model/Meta-Llama-3-8B \
-    --load_quant ../llm-awq/quant_cache/Meta-Llama-3-8B-w4-g128-awq-lutgemm-v2.pt\
+Phi-3-medium-4k-instruct
+
+MODEL=Meta-Llama-3-8B-Instruct
+
+python ./examples/quant_model_rtn.py \
+    --model_name_or_path ../models/$MODEL\
+    --output_name ./outputs/$MODEL-w4-g128-lutgemm.pt\
+    --load_quant ../llm-awq/quant_cache/$MODEL-w4-g128-awq-lutgemm-v2.pt\
     --qbits 4 \
     --group_size 128
+
+
+    
