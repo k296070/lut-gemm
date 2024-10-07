3a9eafece25d4543654c0775e2d88001fec89a35
HOJOONKIM <k296070@snu.ac.kr>
2024-10-07 21:05:35 +0900

    edit

--- examples/rtn_parameter.py
+++ examples/rtn_parameter.py
@@ -47,12 +47,9 @@ class RTNParameter(CompressionParameter):
                 data = data.reshape([-1, group_size])
             quant.find_params(data, weight=True)
             quant_data = torch.clamp(torch.round(data / quant.scale) + quant.zero, 0, quant.maxq)
-            quant_data = quant_data.reshape([in_ch, -1]).to(torch.int).T
-            quant.scale = quant.scale.reshape([in_ch, -1]).T
-            quant.zero  = quant.zero.reshape([in_ch, -1]).T
-
-            quant.scale = quant.scale.reshape([-1,in_ch, 1])
-            quant.zero  = quant.zero.reshape([-1, in_ch, 1])
+            quant_data = quant_data.reshape([in_ch, -1, group_size]).to(torch.int)
+            quant.scale = quant.scale.reshape([in_ch, -1, 1])
+            quant.zero  = quant.zero.reshape([in_ch, -1, 1])
 
         return quant.scale, quant.zero, quant_data, quant_data.shape
 
@@ -80,29 +77,28 @@ class RTNParameter(CompressionParameter):
     def convert_bcq_format(self, scale, zero, quant_data, qbits, do_packing=False, in_ch_wise=False):
         global PACKER
 
-        zero   = scale * zero
+        zero   = scale * zero #O ,#G,1
         upack  = torch.Tensor([[2**i for i in range(qbits)]])
         scale  = scale / 2.0
-        scale  = torch.matmul(scale, upack)
+        scale  = torch.matmul(scale, upack) #O G B
 
-        offset = scale.sum(-1).unsqueeze(-1) - zero
-
+        offset = scale.sum(-1).unsqueeze(-1) - zero #O G 1
+        offset= offset.reshape(offset.shape[0],-1)
         binary = torch.zeros(list(quant_data.shape) + [qbits])
         binary_shape = binary.shape
         for i in range(qbits):
             binary[:, :, i] = ((quant_data >> i) & 1) * 2.0 - 1.0
                     #o,i
-        scale = scale.permute(1,2,0)
-        binary = binary.permute(1,2,0)
-        offset = offset.permute(1,0,2)
-        K = binary.shape[0] #input
-        
-        binary = binary.reshape(K,qbits,-1)
 
-        N = binary.shape[2] #output
+        K = binary.shape[1] #input
+        N = binary.shape[0] #output
+
+        scale = scale.permute(1,2,0) # G B O
+        binary = binary.permute(1,2,0) # I B O
+        offset = offset.permute(1,0) # 
 
         bW = torch.zeros([K // 32, qbits, N], dtype=torch.int64)
-        binary_shape = binary.shape
+    
         if do_packing == True:
             for n in range(N):
                 for b in range(qbits):
@@ -110,7 +106,7 @@ class RTNParameter(CompressionParameter):
                         s = 0
                         for t in range(32):
                             if binary[k + t][b][n] == 1:
-                                s |= 1 << t  # 비트를 설정
+                                s |= (1 << t)  # 비트를 설정
                         bW[k // 32][b][n] = (s & 0xFFFFFFFF)
         bW = bW.to(torch.int32)
         return scale, bW, binary_shape, offset
@@ -122,7 +118,7 @@ if __name__ == '__main__':
     w_rtn = RTNParameter(w_org)
     scale, zero, w_quant, w_quant_shape = w_rtn.compress(in_ch_wise=False, qbits=4, group_size=128, perchannel=True, sym=False)
     #scale, zero, w_quant, w_quant_shape = w_rtn.compress(in_ch_wise=False, qbits=4, group_size=128, perchannel=True, sym=False)
-    print(abs(w_org-w_rtn.data).mean())
+
     print("quant",scale.shape,zero.shape,w_quant.shape)
     # Convert INT4 -> BCQ4
     alpha, binary, binary_shape, offset = w_rtn.convert_bcq_format(scale, zero, w_quant, qbits=4, do_packing=True, in_ch_wise=True)
